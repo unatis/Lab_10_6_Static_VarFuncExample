35ce0986edb5f4f43c4f8651aba48502795e787f
Yuri Goncharov <ygpanzer@gmail.com>
2021-06-02 15:12:10 +0200

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //FirstOne firstOne = new FirstOne();
+
+        FirstOne.SomeFunction();
+
+        FirstOne.Number = 8;
+
+        int tmp = FirstOne.Number;
+
+        System.out.println(tmp);
+
+    }
+}
